c77f249b308abbc584d09d0bd28d016bf91640da
Soumya Mishra <soumyamishra788@gmail.com>
2025-11-05 15:23:14 +0530

    Modification.

--- Edufundr/tailwind.config.js
+++ Edufundr/tailwind.config.js
@@ -1,10 +1,14 @@
-module.exports = {
+/** @type {import('tailwindcss').Config} */
+export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}"
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./public/**/*.html"
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
